589a22e0d12b075916d9b0caba659791e5d0db9c
egonixaimgod <bodnarbence2002@gmail.com>
2023-03-24 09:14:16 +0100

    Megjegyzes kesz, a projekt kesz.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+/*
+* File: app.js
+* Author: Bodnár Bence
+* Copyright: 2023, Bodnár Bence
+* Group: Szoft i/2/n
+* Date: 2023.03.24
+* Github: https://github.com/egonixaimgod/
+*/
+
 aElInput = document.querySelector("#aEl");
 bElInput = document.querySelector("#aEl");
 cElInput = document.querySelector("#aEl");
